b377d7e3f96756de66476ece490f0b9506f8fd99
njerucyrus <njerucyrus5@hotmail.com>
2017-04-21 22:34:12 +0300

    completed update method in patient controller

--- src/Hudutech/Controller/PatientController.php
+++ src/Hudutech/Controller/PatientController.php
@@ -80,7 +80,51 @@ class PatientController implements PatientInterface
 
     public function update(Patient $patient, $id)
     {
-        // TODO: Implement update() method.
+        $db = new DB();
+        $conn = $db->connect();
+
+        $patientNo = $patient->getPatientNo();
+        $sirName = $patient->getSirName();
+        $idNo = $patient->getIdNo();
+        $firstName = $patient->getFirstName();
+        $otherName = $patient->getOtherName();
+        $maritalStatus = $patient->getMaritalStatus();
+        $phoneNumber = $patient->getPhoneNumber();
+        $occupation = $patient->getOccupation();
+        $patientType = $patient->getPatientType();
+        $sex = $patient->getSex();
+
+        try{
+
+            $stmt = $conn->prepare("UPDATE patients SET
+                                                        patient_no=:patient_no,
+                                                        id_no=:id_no,
+                                                        sir_name=:sir_name,
+                                                        first_name=:first_name, 
+                                                        marital_status=:marital_status,
+                                                        phone_number=:phone_number,
+                                                        occupation=:occupation,
+                                                        patient_type=:patient_type,
+                                                        sex=:sex, 
+                                                        other_name=:other_name
+                                                     WHERE id=:id
+                                                     ");
+
+            $stmt->bindParam(":id",$id);
+            $stmt->bindParam(":patient_no",$patientNo);
+            $stmt->bindParam(":id_no",$idNo);
+            $stmt->bindParam(":sir_name",$sirName);
+            $stmt->bindParam(":first_name",$firstName);
+            $stmt->bindParam(":other_name",$otherName);
+            $stmt->bindParam(":marital_status",$maritalStatus);
+            $stmt->bindParam(":phone_number",$phoneNumber);
+            $stmt->bindParam(":occupation",$occupation);
+            $stmt->bindParam(":patient_type",$patientType);
+            $stmt->bindParam(":sex",$sex);
+            return $stmt->execute() ? true : false;
+        } catch (\PDOException $exception){
+            return false;
+        }
     }
 
     public static function delete($id)
