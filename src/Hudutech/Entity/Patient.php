30742d5014d639991add16da8287746778162b74
njerucyrus <njerucyrus5@hotmail.com>
2017-04-21 20:18:41 +0300

    added db manager dir and completed patient entity

--- src/Hudutech/Entity/Patient.php
+++ src/Hudutech/Entity/Patient.php
@@ -11,5 +11,186 @@ namespace Hudutech\Entity;
 
 class Patient
 {
+    /**
+     * @var string
+     */
+    private $patientNo;
+    /**
+     * @var integer
+     */
+    private $idNo;
+    /**
+     * @var string
+     */
+    private $sirName;
+    /**
+     * @var string
+     */
+    private $firstName;
+    /**
+     * @var string
+     */
+    private $otherName;
+    /**
+     * @var string
+     */
+    private $sex;
+    /**
+     * @var string
+     */
+    private $maritalStatus;
+    /**
+     * @var string
+     */
+    private $phoneNumber;
+    /**
+     * @var string
+     */
+    private $patientType;
+
+    /**
+     * @return string
+     */
+    public function getPatientNo()
+    {
+        return $this->patientNo;
+    }
+
+    /**
+     * @param string $patientNo
+     */
+    public function setPatientNo($patientNo)
+    {
+        $this->patientNo = $patientNo;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIdNo()
+    {
+        return $this->idNo;
+    }
+
+    /**
+     * @param int $idNo
+     */
+    public function setIdNo($idNo)
+    {
+        $this->idNo = $idNo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSirName()
+    {
+        return $this->sirName;
+    }
+
+    /**
+     * @param string $sirName
+     */
+    public function setSirName($sirName)
+    {
+        $this->sirName = $sirName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOtherName()
+    {
+        return $this->otherName;
+    }
+
+    /**
+     * @param string $otherName
+     */
+    public function setOtherName($otherName)
+    {
+        $this->otherName = $otherName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSex()
+    {
+        return $this->sex;
+    }
+
+    /**
+     * @param string $sex
+     */
+    public function setSex($sex)
+    {
+        $this->sex = $sex;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMaritalStatus()
+    {
+        return $this->maritalStatus;
+    }
+
+    /**
+     * @param string $maritalStatus
+     */
+    public function setMaritalStatus($maritalStatus)
+    {
+        $this->maritalStatus = $maritalStatus;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+
+    /**
+     * @param string $phoneNumber
+     */
+    public function setPhoneNumber($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPatientType()
+    {
+        return $this->patientType;
+    }
+
+    /**
+     * @param string $patientType
+     */
+    public function setPatientType($patientType)
+    {
+        $this->patientType = $patientType;
+    }
+
 
 }
