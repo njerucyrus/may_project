df671341ddf4e9b0354ba162ba144b4bddee3549
njerucyrus <njerucyrus5@hotmail.com>
2017-04-21 20:23:11 +0300

    completed patient interface crud method

--- src/Hudutech/AppInterface/PatientInterface.php
+++ src/Hudutech/AppInterface/PatientInterface.php
@@ -9,7 +9,44 @@
 namespace Hudutech\AppInterface;
 
 
+use Hudutech\Entity\Patient;
+
 interface PatientInterface
 {
+    /**
+     * @param Patient $patient
+     * @return boolean
+     */
+    public function create(Patient $patient);
 
+    /**
+     * @param Patient $patient
+     * @param $id
+     * @return boolean
+     */
+    public function update(Patient $patient, $id);
+
+    /**
+     * @param $id
+     * @return boolean
+     */
+    public static function delete($id);
+
+
+    /**
+     * @return boolean
+     * delete all data from patients table
+     */
+    public static function destroy();
+
+    /**
+     * @param $id
+     * @return array
+     */
+    public static function getId($id);
+
+    /**
+     * @return array
+     */
+    public static function all();
 }
