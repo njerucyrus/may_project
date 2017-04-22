957704c194c6c4632b4d04661deebd49afd5eaa5
njerucyrus <njerucyrus5@hotmail.com>
2017-04-22 20:39:49 +0300

    completed create method in PatientTest ctrl

--- src/Hudutech/Controller/PatientClinicalTestController.php
+++ src/Hudutech/Controller/PatientClinicalTestController.php
@@ -10,13 +10,52 @@ namespace Hudutech\Controller;
 
 
 use Hudutech\AppInterface\PatientClinicalTestInterface;
+use Hudutech\DBManager\DB;
 use Hudutech\Entity\PatientClinicalTest;
 
 class PatientClinicalTestController implements PatientClinicalTestInterface
 {
     public function create(PatientClinicalTest $patientClinicalTest)
     {
-        // TODO: Implement create() method.
+        $db = new DB();
+        $conn = $db->connect();
+
+        $clinicianId = $patientClinicalTest->getClinicianId();
+        $patientId = $patientClinicalTest->getPatientId();
+        $testId = $patientClinicalTest->getTestId();
+        $testResult = $patientClinicalTest->getTestResult();
+        $description = $patientClinicalTest->getDescription();
+
+        try {
+
+            $sql = "INSERT INTO patient_clinical_tests(
+                                                        clinician_id,
+                                                        patient_id,
+                                                        test_id,
+                                                        test_result,
+                                                        description
+                                                      ) 
+                                                VALUES
+                                                 (
+                                                        :clinician_id,
+                                                        :patient_id,
+                                                        :test_id,
+                                                        :test_result,
+                                                        :description
+                                                 )";
+
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(":clinician_id", $clinicianId);
+            $stmt->bindParam(":patient_id", $patientId);
+            $stmt->bindParam(":test_id", $testId);
+            $stmt->bindParam(":test_result", $testResult);
+            $stmt->bindParam(":description", $description);
+            return $stmt->execute() ? true : false;
+
+        } catch (\PDOException $exception) {
+            echo $exception->getMessage();
+            return false;
+        }
     }
 
     public function update(PatientClinicalTest $patientClinicalTest, $id)
