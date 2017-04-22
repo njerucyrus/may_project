ec8a3383c38b8b7044e77967579201a3e1baaddf
njerucyrus <njerucyrus5@hotmail.com>
2017-04-22 16:10:33 +0300

    completed update delete and destroy methods

--- src/Hudutech/Controller/ClinicalTestController.php
+++ src/Hudutech/Controller/ClinicalTestController.php
@@ -39,17 +39,52 @@ class ClinicalTestController implements ClinicalTestInterface
 
     public function update(ClinicalTest $clinicalTest, $id)
     {
-        // TODO: Implement update() method.
+        $db = new DB();
+        $conn= $db->connect();
+        $testName = $clinicalTest->getTestName();
+        $cost = $clinicalTest->getCost();
+
+        try{
+            $sql = "UPDATE clinical_tests SET test_name=:test_name, cost=:cost WHERE id=:id";
+
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(":id", $id);
+            $stmt->bindParam(":test_name", $testName);
+            $stmt->bindParam(":cost", $cost);
+            return $stmt->execute() ? true : false;
+
+        }catch (\PDOException $exception) {
+            echo $exception->getMessage();
+            return false;
+        }
+
     }
 
     public static function delete($id)
     {
-        // TODO: Implement delete() method.
+        $db = new DB();
+        $conn = $db->connect();
+        try{
+            $stmt = $conn->prepare("DELETE FROM clinical_tests WHERE id=:id");
+            $stmt->bindParam(":id", $id);
+            return $stmt->execute() ? true : false;
+        } catch (\PDOException $exception) {
+            echo $exception->getMessage();
+            return false;
+        }
     }
 
     public static function destroy()
     {
-        // TODO: Implement destroy() method.
+        $db = new DB();
+        $conn = $db->connect();
+        try{
+            $stmt = $conn->prepare("DELETE FROM clinical_tests ");
+            return $stmt->execute() ? true : false;
+        } catch (\PDOException $exception) {
+            echo $exception->getMessage();
+            return false;
+        }
     }
 
     public static function getId($id)
