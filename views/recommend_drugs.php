1a1eb881030533df4450f096f348d7857b101e76
Samuel Githae <samgithae01@gmail.com>
2017-05-11 12:29:37 +0300

    added recommend drug  ui

--- views/recommend_drugs.php
+++ views/recommend_drugs.php
@@ -27,7 +27,7 @@
                         <div class="panel-title col-md-offset-3">
 
 
-                            <h1>Drug Prescription</h1>
+                            <h1>Drug Recommendation</h1>
                             <div id="feedback">
 
                             </div>
@@ -99,9 +99,149 @@
 
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="panel panel-primary" data-collapsed="0">
+
+                    <div class="panel-heading">
+                        <div class="panel-title col-md-offset-3">
+
+
+                            <h3> Add Drugs</h3>
+
+                        </div>
+
+
+                    </div>
+
+                    <div class="panel-body ">
+
+                        <!--                   body content will start here-->
+
+
+
+                            <form role="form" class="form-groups-bordered">
+                                <div class="row  container-fluid">
+
+
+                                <div class="form-group col-xs-3 col-md-3" style="padding: 5px; margin: 5px;">
+                                    <label for="drugName"style="padding-left: 10px;" class="control-label">Drug Name</label>
+
+
+                                        <input type="text" class="form-control" id="drugName" placeholder="Drug Name">
+
+                                </div>
+                                <div class="form-group col-xs-2 col-md-2" style="padding: 5px; margin: 5px;">
+                                    <label for="type"style="padding-left: 10px;" class="control-label">Administration Type</label>
+
+
+                                    <input type="text" class="form-control" id="type" placeholder="type">
+
+                                </div>
+                                <div class="form-group col-xs-1 col-md-1" style="padding: 5px; margin: 5px;">
+                                    <label for="dosage"style="padding-left: 10px;" class="control-label">Dosage</label>
+
+
+                                    <input type="text" class="form-control" id="dosage" placeholder="Dosage">
+
+                                </div>
+                                <div class="form-group col-xs-3 col-md-3" style=" display:table;padding: 5px; margin: 5px;">
+                                   <label for="prescription"style=" width:100%; padding-right: 50%" class="control-label">Drug Prescription</label>
+
+                                    <select style="width:40%" class="form-control form-horizontal col-md-1" >
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                        <option>6</option>
+                                        <option>7</option>
+                                        <option>8</option>
+                                        <option>9</option>
+                                        <option>10</option>
+                                    </select>
+
+                                    <input type="text"  style="padding-top:10px; width:20%; font-size: xx-large;"class="form-control form-horizontal col-md-1" id="drugName" placeholder="*" disabled>
+
+
+                                    <select style="width:40%" class="form-control form-horizontal col-md-1" >
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                        <option>6</option>
+                                        <option>7</option>
+                                        <option>8</option>
+                                        <option>9</option>
+                                        <option>10</option>
+                                    </select>
+
+                                </div>
+
+                                    <div class="form-group col-xs-1 col-md-1" style="margin-top:30px; ">
+                                        <!--    buttons-->
+
+                                        <input value="Add" id="btn-add-recommend" onclick="submitFormData()"
+                                               class="btn btn-green  btn-md" type="button">
+
+                                    </div>
+
+
+                                </div>
+
+
+
+                            </form>
+
+
+                        <!--                        body content will stop here-->
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="panel panel-primary" data-collapsed="0">
+
+                    <div class="panel-heading">
+                        <div class="panel-title col-md-offset-3">
+
+
+                            <h3> Prescribed Drugs</h3>
+
+                        </div>
+
+
+                    </div>
+
+                    <div class="panel-body ">
+
+                        <!--                   body content will start here-->
+
+
+
+
+
+
+                        <!--                        body content will stop here-->
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
     </div>
 </div>
 
-
+<?php
+include 'footer_views.php';
+?>
 </body>
 </html>
