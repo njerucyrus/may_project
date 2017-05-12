344dc50f6ffdce32e780b74eb04d860cc4a43be8
Samuel Githae <samgithae01@gmail.com>
2017-05-12 09:04:01 +0300

    rectrified base ui

--- base.php
+++ base.php
@@ -8,17 +8,16 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
 <?php
 include 'views/head.php';
 ?>
-</head>
 <body class="page-body skin-facebook" >
 
+<div class="page-container"><!-- add class "sidebar-collapsed" to close sidebar by default, "chat-visible" to make chat appear always -->
 
-<div class="page-container">
-    <!-- add class "sidebar-collapsed" to close sidebar by default, "chat-visible" to make chat appear always -->
-    <?php include 'views/right_menu.php'; ?>
+    <?php
+    include 'views/right_menu.php';
+    ?>
 
     <div class="main-content">
 
@@ -35,8 +34,7 @@ include 'views/head.php';
                         <!-- Raw Notifications -->
                         <li class="notifications dropdown">
 
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown"
-                               data-close-others="true">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
                                 <i class="entypo-attention"></i>
                                 <span class="badge badge-info">6</span>
                             </a>
@@ -147,8 +145,7 @@ include 'views/head.php';
                         <!-- Message Notifications -->
                         <li class="notifications dropdown">
 
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown"
-                               data-close-others="true">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
                                 <i class="entypo-mail"></i>
                                 <span class="badge badge-secondary">10</span>
                             </a>
@@ -158,8 +155,7 @@ include 'views/head.php';
                                     <form class="top-dropdown-search">
 
                                         <div class="form-group">
-                                            <input type="text" class="form-control" placeholder="Search anything..."
-                                                   name="s"/>
+                                            <input type="text" class="form-control" placeholder="Search anything..." name="s" />
                                         </div>
 
                                     </form>
@@ -168,8 +164,7 @@ include 'views/head.php';
                                         <li class="active">
                                             <a href="#">
 											<span class="image pull-right">
-												<img src="public/assets/images/thumb-1@2x.png" width="44" alt=""
-                                                     class="img-circle"/>
+												<img src="public/assets/images/thumb-1@2x.png" width="44" alt="" class="img-circle" />
 											</span>
 
                                                 <span class="line">
@@ -186,8 +181,7 @@ include 'views/head.php';
                                         <li class="active">
                                             <a href="#">
 											<span class="image pull-right">
-												<img src="public/assets/images/thumb-2@2x.png" width="44" alt=""
-                                                     class="img-circle"/>
+												<img src="public/assets/images/thumb-2@2x.png" width="44" alt="" class="img-circle" />
 											</span>
 
                                                 <span class="line">
@@ -204,8 +198,7 @@ include 'views/head.php';
                                         <li>
                                             <a href="#">
 											<span class="image pull-right">
-												<img src="public/assets/images/thumb-3@2x.png" width="44" alt=""
-                                                     class="img-circle"/>
+												<img src="public/assets/images/thumb-3@2x.png" width="44" alt="" class="img-circle" />
 											</span>
 
                                                 <span class="line">
@@ -222,8 +215,7 @@ include 'views/head.php';
                                         <li>
                                             <a href="#">
 											<span class="image pull-right">
-												<img src="public/assets/images/thumb-4@2x.png" width="44" alt=""
-                                                     class="img-circle"/>
+												<img src="public/assets/images/thumb-4@2x.png" width="44" alt="" class="img-circle" />
 											</span>
 
                                                 <span class="line">
@@ -248,6 +240,7 @@ include 'views/head.php';
 
                         <!-- Task Notifications -->
 
+
                     </ul>
 
             </div>
@@ -263,37 +256,37 @@ include 'views/head.php';
 
                         Language: &nbsp;
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-close-others="true">
-                            <img src="public/assets/images/flags/flag-uk.png" width="16" height="16"/>
+                            <img src="public/assets/images/flags/flag-uk.png" width="16" height="16" />
                         </a>
 
                         <ul class="dropdown-menu pull-right">
                             <li>
                                 <a href="#">
-                                    <img src="public/assets/images/flags/flag-de.png" width="16" height="16"/>
+                                    <img src="public/assets/images/flags/flag-de.png" width="16" height="16" />
                                     <span>Deutsch</span>
                                 </a>
                             </li>
                             <li class="active">
                                 <a href="#">
-                                    <img src="public/assets/images/flags/flag-uk.png" width="16" height="16"/>
+                                    <img src="public/assets/images/flags/flag-uk.png" width="16" height="16" />
                                     <span>English</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#">
-                                    <img src="public/assets/images/flags/flag-fr.png" width="16" height="16"/>
+                                    <img src="public/assets/images/flags/flag-fr.png" width="16" height="16" />
                                     <span>François</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#">
-                                    <img src="public/assets/images/flags/flag-al.png" width="16" height="16"/>
+                                    <img src="public/assets/images/flags/flag-al.png" width="16" height="16" />
                                     <span>Shqip</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#">
-                                    <img src="public/assets/images/flags/flag-es.png" width="16" height="16"/>
+                                    <img src="public/assets/images/flags/flag-es.png" width="16" height="16" />
                                     <span>Español</span>
                                 </a>
                             </li>
@@ -326,7 +319,10 @@ include 'views/head.php';
 
         </div>
 
-        <hr/>
+        <hr />
+
+
+
 
 
         <div class="row">
@@ -334,9 +330,7 @@ include 'views/head.php';
 
                 <div class="tile-stats tile-red">
                     <div class="icon"><i class="entypo-users"></i></div>
-                    <div class="num" data-start="0" data-end="83" data-postfix="" data-duration="1500" data-delay="0">
-                        0
-                    </div>
+                    <div class="num" data-start="0" data-end="83" data-postfix="" data-duration="1500" data-delay="0">0</div>
 
                     <h3>Registered users</h3>
                     <p>so far in our Hospital System</p>
@@ -360,7 +354,7 @@ include 'views/head.php';
 
             <div class="col-sm-3 col-xs-6">
 
-
+                <div class="tile-stats tile-aqua">
                     <div class="icon" style="padding-bottom: 40px;"><i class="fa fa-user-md"></i></div>
                     <div class="num" data-start="0" data-end="1023" data-postfix="" data-duration="1500" data-delay="1200">0</div>
 
@@ -383,20 +377,113 @@ include 'views/head.php';
             </div>
         </div>
 
-        <br/>
+        <br />
 
-<!--       down row start here-->
+        <!--       down row start here-->
 
-<!--        down row stop here-->
-
-
-        <br/>
+        <!--        down row stop here-->
 
 
-        <br/>
+        <br />
 
 
 
+        <br />
+
+
+
+
+
+        <!-- Footer -->
+        <footer class="main">
+
+            &copy; 2017 <strong>Developed by</strong>  <a href="http://hudutech.com" target="_blank">Hudutech Solutions</a>
+
+        </footer>
+    </div>
+
+
+    <div id="chat" class="fixed" data-current-user="Art Ramadani" data-order-by-status="1" data-max-chat-history="25">
+
+        <div class="chat-inner">
+
+
+            <h2 class="chat-header">
+                <a href="#" class="chat-close"><i class="entypo-cancel"></i></a>
+
+                <i class="entypo-users"></i>
+                Chat
+                <span class="badge badge-success is-hidden">0</span>
+            </h2>
+
+
+            <div class="chat-group" id="group-1">
+                <strong>Favorites</strong>
+
+                <a href="#" id="sample-user-123" data-conversation-history="#sample_history"><span class="user-status is-online"></span> <em>Catherine J. Watkins</em></a>
+                <a href="#"><span class="user-status is-online"></span> <em>Nicholas R. Walker</em></a>
+                <a href="#"><span class="user-status is-busy"></span> <em>Susan J. Best</em></a>
+                <a href="#"><span class="user-status is-offline"></span> <em>Brandon S. Young</em></a>
+                <a href="#"><span class="user-status is-idle"></span> <em>Fernando G. Olson</em></a>
+            </div>
+
+
+            <div class="chat-group" id="group-2">
+                <strong>Work</strong>
+
+                <a href="#"><span class="user-status is-offline"></span> <em>Robert J. Garcia</em></a>
+                <a href="#" data-conversation-history="#sample_history_2"><span class="user-status is-offline"></span> <em>Daniel A. Pena</em></a>
+                <a href="#"><span class="user-status is-busy"></span> <em>Rodrigo E. Lozano</em></a>
+            </div>
+
+
+            <div class="chat-group" id="group-3">
+                <strong>Social</strong>
+
+                <a href="#"><span class="user-status is-busy"></span> <em>Velma G. Pearson</em></a>
+                <a href="#"><span class="user-status is-offline"></span> <em>Margaret R. Dedmon</em></a>
+                <a href="#"><span class="user-status is-online"></span> <em>Kathleen M. Canales</em></a>
+                <a href="#"><span class="user-status is-offline"></span> <em>Tracy J. Rodriguez</em></a>
+            </div>
+
+        </div>
+
+        <!-- conversation template -->
+        <div class="chat-conversation">
+
+            <div class="conversation-header">
+                <a href="#" class="conversation-close"><i class="entypo-cancel"></i></a>
+
+                <span class="user-status"></span>
+                <span class="display-name"></span>
+                <small></small>
+            </div>
+
+            <ul class="conversation-body">
+            </ul>
+
+            <div class="chat-textarea">
+                <textarea class="form-control autogrow" placeholder="Type your message"></textarea>
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+
+
+</div>
+
+
+
+<!-- Imported styles on this page -->
+<link rel="stylesheet" href="public/assets/js/jvectormap/jquery-jvectormap-1.2.2.css">
+<link rel="stylesheet" href="public/assets/js/rickshaw/rickshaw.min.css">
+
+<!-- Bottom scripts (common) -->
+<script src="public/assets/js/gsap/TweenMax.min.js"></script>
 <script src="public/assets/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js"></script>
 <script src="public/assets/js/jquery-3.2.0.slim.min.js"></script>
 <script src="public/assets/js/bootstrap.js"></script>
