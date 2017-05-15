28fd13b02b817965feac1d7821763cbada9f32dd
njerucyrus <njerucyrus5@hotmail.com>
2017-05-15 23:49:34 +0300

    added DrugSales

---
+++ src/Hudutech/Entity/DrugSales.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hudutech
+ * Date: 5/15/17
+ * Time: 11:47 PM
+ */
+
+namespace Hudutech\Entity;
+
+
+class DrugSales
+{
+    private $id;
+    private $patientNo;
+    private $inventoryId;
+    private $qty;
+    private $price;
+    private $datePurchased;
+    private $servedBy;
+}
