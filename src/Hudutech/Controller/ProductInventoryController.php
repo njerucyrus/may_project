33f17333c638ac99de2f373b4f3ebb9994336497
njerucyrus <njerucyrus5@hotmail.com>
2017-05-16 09:42:45 +0300

    completed update, delete, getId and getObject methods in Product inventory ctrl

--- src/Hudutech/Controller/ProductInventoryController.php
+++ src/Hudutech/Controller/ProductInventoryController.php
@@ -69,27 +69,96 @@ class ProductInventoryController implements ProductInventoryInterface
 
     public function update(ProductInventory $productInventory, $id)
     {
+        $db = new DB();
+        $conn = $db->connect();
 
+        $batchNo = $productInventory->getBatchNo();
+        $invoiceNo = $productInventory->getInvoiceNo();
+        $productName = $productInventory->getProductName();
+        $qtyReceived = $productInventory->getQtyReceived();
+        $purchasePrice = $productInventory->getPurchasePrice();
+        $salePrice = $productInventory->getSalePrice();
+        $purchaseDate = $productInventory->getPurchaseDate();
+        $expiryDate = $productInventory->getExpiryDate();
+
+        try{
+            $stmt = $conn->prepare("UPDATE product_inventory SET
+                                                                   batchNo=:batchNo,
+                                                                    invoiceNo=:invoiceNo, 
+                                                                    productName=:productName,
+                                                                    qtyReceived=:qtyReceived,
+                                                                    purchasePrice=:purchasePrice, 
+                                                                    salePrice=:salePrice, 
+                                                                    purchaseDate=:purchaseDate, 
+                                                                    expiryDate=:expiryDate
+                                                                WHERE id=:id
+                                                                
+                                                               ");
+            $stmt->bindParam(":id",$id);
+            $stmt->bindParam(":batchNo",$batchNo);
+            $stmt->bindParam(":invoiceNo", $invoiceNo);
+            $stmt->bindParam(":productName", $productName);
+            $stmt->bindParam(":qtyReceived", $qtyReceived);
+            $stmt->bindParam(":purchasePrice", $purchasePrice);
+            $stmt->bindParam(":salePrice", $salePrice);
+            $stmt->bindParam(":purchaseDate", $purchaseDate);
+            $stmt->bindParam(":expiryDate", $expiryDate);
+            return $stmt->execute() ? true : false;
+        } catch (\PDOException $exception) {
+            echo $exception->getMessage();
+            return false;
+        }
     }
 
     public static function delete($id)
     {
-        // TODO: Implement delete() method.
+        $db = new DB();
+        $conn = $db->connect();
+
+        try{
+            $stmt = $conn->prepare("DELETE FROM product_inventory WHERE id=:id");
+            $stmt->bindParam(":id", $id);
+            return $stmt->execute() ? true :false;
+        } catch (\PDOException $exception) {
+            echo $exception->getMessage();
+            return false;
+        }
     }
 
     public static function getId($id)
     {
-        // TODO: Implement getId() method.
+        $db = new DB();
+        $conn = $db->connect();
+
+        try{
+            $stmt = $conn->prepare("SELECT t.* FROM product_inventory t WHERE t.id=:id");
+            $stmt->bindParam(":id", $id);
+            return $stmt->execute() && $stmt->rowCount() ? $stmt->fetch(\PDO::FETCH_ASSOC) : [];
+        } catch (\PDOException $exception) {
+            echo $exception->getMessage();
+            return [];
+        }
     }
 
     public static function getObject($id)
     {
-        // TODO: Implement getObject() method.
+        $db = new DB();
+        $conn = $db->connect();
+
+        try{
+            $stmt = $conn->prepare("SELECT t.* FROM product_inventory t WHERE t.id=:id");
+            $stmt->bindParam(":id", $id);
+            $stmt->setFetchMode(\PDO::FETCH_CLASS |\PDO::FETCH_PROPS_LATE, ProductInventory::class);
+            return $stmt->execute() && $stmt->rowCount() ? $stmt->fetch() : null;
+        } catch (\PDOException $exception) {
+            echo $exception->getMessage();
+            return null;
+        }
     }
 
     public static function all()
     {
-        // TODO: Implement all() method.
+
     }
 
     public static function getPrice($id, $qty)
